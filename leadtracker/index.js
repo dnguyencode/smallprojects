74849f03d34c4a08befcea74ee2808702e5ee3a9
dnguyencode <daniel.nguyen.qc@gmail.com>
2021-12-10 03:36:18 -0500

    complete lead tracker app

--- leadtracker/index.js
+++ leadtracker/index.js
@@ -4,11 +4,18 @@ const inputBtn = document.getElementById("input-btn");
 const ulEL = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        addLead(tabs[0].url)
+    })
+})
 
 function render(leads) {
     let listItems = "";
@@ -24,10 +31,14 @@ function render(leads) {
     ulEL.innerHTML = listItems;
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
+function addLead(lead) {
+    myLeads.push(lead);
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads);
+}
+
+inputBtn.addEventListener("click", function() {
+    addLead(inputEl.value);
     inputEl.value = "";
 })
 
